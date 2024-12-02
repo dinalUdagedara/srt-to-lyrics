b7d1bac4094a88a7822bd0393420574fec5ab9c1
Thiyunu <dinal.bandara@gmail.com>
2024-12-02 21:22:28 +0530

    fix: removed unnecessary comments

--- components/player.tsx
+++ components/player.tsx
@@ -1,15 +1,7 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
-import SrtParser2 from "srt-parser-2";
+import React from "react";
 import MusicPlayerCard from "./music-player-card";
-import AlbumCoverImage from "@/public/assets/start-boy-cover.png"
-
-type Subtitle = {
-  id: string;
-  startTime: string;
-  endTime: string;
-  text: string;
-};
+import AlbumCoverImage from "@/public/assets/start-boy-cover.png";
 
 const LyricsPlayer = ({
   srtContent,
@@ -18,59 +10,8 @@ const LyricsPlayer = ({
   srtContent: string;
   audioSrc: string;
 }) => {
-  // const [subtitles, setSubtitles] = useState<Subtitle[]>([]);
-  // const [currentLyric, setCurrentLyric] = useState<string>("");
-  // const audioRef = useRef<HTMLAudioElement | null>(null);
-
-  // // Parse the SRT content when the component mounts
-  // useEffect(() => {
-  //   const parser = new SrtParser2();
-  //   const parsedSrt = parser.fromSrt(srtContent); // Parse the SRT file into an array of subtitles
-  //   setSubtitles(parsedSrt);
-  // }, [srtContent]);
-
-  // // Convert SRT time format (HH:MM:SS,MS) to milliseconds for comparison
-  // const timeToMilliseconds = (time: string) => {
-  //   const [hours, minutes, seconds] = time.split(":");
-  //   const [secs, millis] = seconds.split(",");
-  //   return (
-  //     parseInt(hours, 10) * 3600000 +
-  //     parseInt(minutes, 10) * 60000 +
-  //     parseInt(secs, 10) * 1000 +
-  //     parseInt(millis, 10)
-  //   );
-  // };
-
-  // // Update the lyrics based on the current time of the audio
-  // const updateLyric = () => {
-  //   if (audioRef.current) {
-  //     const currentTime = audioRef.current.currentTime * 1000; // Convert seconds to milliseconds
-  //     const currentSubtitle = subtitles.find(
-  //       (subtitle) =>
-  //         currentTime >= timeToMilliseconds(subtitle.startTime) &&
-  //         currentTime <= timeToMilliseconds(subtitle.endTime)
-  //     );
-  //     if (currentSubtitle) {
-  //       setCurrentLyric(currentSubtitle.text);
-  //     } else {
-  //       setCurrentLyric(""); // Clear lyrics when there's no matching subtitle
-  //     }
-  //   }
-  // };
-
   return (
     <div>
-      {/* <audio
-        ref={audioRef}
-        src={audioSrc}
-        controls
-        onTimeUpdate={updateLyric}
-      />
-
-      <div className="mt-10">
-        <h2>{currentLyric}</h2>
-      </div> */}
-
       {/* <MusicPlayerCard /> */}
       <MusicPlayerCard
         srtContent={srtContent}
