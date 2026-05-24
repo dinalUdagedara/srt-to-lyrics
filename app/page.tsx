6f3b7baa8b6c7481e0461911cf51e53487cfae80
Dinal Udagedara <126741968+dinalUdagedara@users.noreply.github.com>
2026-05-24 20:09:38 +0530

    feat: integrate srt-lyric-player into demo app with dark gradient theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,10 @@
 "use client";
-import { useEffect, useState } from "react";
 import LyricsPlayer from "@/components/player";
 
 export default function Home() {
-  const [srtContent, setSrtContent] = useState<string>("");
-
-  useEffect(() => {
-    fetch("/assets/lonely-night.srt")
-      .then((res) => res.text())
-      .then((data) => setSrtContent(data));
-  }, []);
-
   return (
-    <div className="flex flex-col w-full justify-center  items-center gap-10 pt-10">
-      {srtContent && (
-        <LyricsPlayer
-          srtContent={srtContent}
-          audioSrc="/assets/lonely-night.mp3"
-        />
-      )}
+    <div className="flex flex-col w-full justify-center items-center gap-10 pt-10">
+      <LyricsPlayer audioSrc="/assets/lonely-night.mp3" />
     </div>
   );
 }
